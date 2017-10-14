a0c22307dd0233b7b390aed69f4326b2988dc028
Deyl <soulvisman@gmail.com>
2017-10-14 16:36:27 +0600

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project which handle GET request and return JSON object which contaion unixtime and human time. 
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.end('Initialized');
+});
+
+app.listen(3000, 'Server is working')
