0704a8504d9589a662238f29417ebf23beeef478
Deyl <soulvisman@gmail.com>
2017-10-14 17:55:45 +0600

    some features

--- server.js
+++ server.js
@@ -6,7 +6,25 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.end('Initialized');
+  res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/:typedDate', (req, res) => {
+  const typedDate = req.params.typedDate;
+  let timestamp = {};
+  if (isNaN(typedDate)){
+    timestamp.unix = new Date(typedDate).getTime() / 1000.0;
+    timestamp.natural = typedDate;
+  } else {
+    let natural = new Date(parseInt(typedDate) * 1000);
+    const monthName = [
+      'January', 'February', 'March', 'April', 'May', 'June',
+      'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    timestamp.unix = parseInt(typedDate);
+    timestamp.natural = monthName[natural.getMonth()] + ' ' +
+                        natural.getDate() + ', ' + natural.getFullYear();
+  }
+  res.json(timestamp);
 });
 
-app.listen(3000, 'Server is working')
+app.listen(3000);
